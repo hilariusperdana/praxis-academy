f4a5e3ae449cb0d0f1e46f57d2fdf669e846c229
hilariusperdana <hilarius.perdana@gmail.com>
2021-02-09 15:42:42 +0700

    commit

--- novice/01-02/coba_modul.py
+++ novice/01-02/coba_modul.py
@@ -1,1 +1,17 @@
-import m_bubblesort
+#pilih salah satu
+
+from m_insertionsort import InsertionSort
+from m_bubblesort import BubbleSort
+from m_mergesort import mergesort
+from m_quicksort import QuickSort
+from m_selesctionsort import selectionsort
+
+#masukkan data dan jenis sorting
+objek = ['data']
+'jenis sorting'(objek)
+print("hasil",objek)
+
+#khusus quicksort format seperti berikut
+objek = ['data']
+QuickSort(objek,0,len(objek)-1)
+print("hasil",objek)
--- novice/01-02/cobadictionary.py
+++ novice/01-02/cobadictionary.py
@@ -1,3 +1,8 @@
 angka=[1,2,3,4,5]
 for x in angka:
     print (x)
+
+
+objek = [1,2,7,4,5,7,3,9,1,9,4,6,8]
+InsertionSort(objek)
+print("hasil",objek)
