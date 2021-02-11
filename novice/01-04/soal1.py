f74f8f99869227519b43255892b5b1e8204ac3ec
hilariusperdana <hilarius.perdana@gmail.com>
2021-02-11 13:31:10 +0700

    commit

---
+++ novice/01-04/soal1.py
@@ -0,0 +1,16 @@
+#soal no 1
+a=2
+b=5
+
+print(int(a/b))
+print(float(a/b))
+
+#soal no 2
+firstname='hilarius'
+lastname='perdana'
+print('Hello Praxis, saya',firstname,lastname,'! saya siap belajar enterprise python developer.')
+
+#soal no 3
+p = 9.99999
+q = 'the number: '
+print('jawaban',q,p)
