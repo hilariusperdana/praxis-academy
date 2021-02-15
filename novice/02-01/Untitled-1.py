4a52755e9b251dc34d43bf9a3c6316878ad24346
hilariusperdana <hilarius.perdana@gmail.com>
2021-02-15 11:57:08 +0700

    commit

---
+++ novice/02-01/Untitled-1.py
@@ -0,0 +1,10 @@
+import pickle 
+object = Object () 
+filehandler = buka (nama file, 'w') 
+pickle.dump (objek, filehandler)
+
+import pickle 
+import math 
+object_pi = math.pi 
+file_pi = open ('filename_pi.obj', 'w') 
+pickle.dump (object_pi, file_pi)
