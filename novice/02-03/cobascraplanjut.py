f7edb26b51bb96d4c428d121ccd75e42e6abd9c1
hilariusperdana <hilarius.perdana@gmail.com>
2021-02-17 15:35:07 +0700

    belum fix

---
+++ novice/02-03/cobascraplanjut.py
@@ -0,0 +1,6 @@
+from cobascrap import simple_get
+raw_html=simple_get('https://realpython.com/blog/')
+len(raw_html)
+no_html=simple_get('https://realpython.com/blog/nope-not-gonna-find-it')
+no_html is None
+True
