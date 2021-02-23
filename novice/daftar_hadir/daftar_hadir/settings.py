5895adc0c10efb6c6d452ec2abe2b075a3c4665e
hilariusperdana <hilarius.perdana@gmail.com>
2021-02-23 17:09:59 +0700

    commit

--- novice/daftar_hadir/daftar_hadir/settings.py
+++ novice/daftar_hadir/daftar_hadir/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'daftar_hadir.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'daftarhadir',
+        'USER': 'hilarius',
+        'PASSWORD': '121996',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
