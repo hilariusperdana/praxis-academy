722648c495e48262dd2e879260521a8dd98a5033
hilariusperdana <hilarius.perdana@gmail.com>
2021-02-26 17:00:39 +0700

    commit

--- novice/daftar_hadir/list_pengunjung/views.py
+++ novice/daftar_hadir/list_pengunjung/views.py
@@ -29,12 +29,12 @@ def hapus(req, id):
 def edit(req, id):
     data = Registrasi.objects.get(id=id)
     if req.POST:
-        Registrasi.objects.get(pk=id).update(
+        Registrasi.objects.filter(pk=id).update(
             nama = req.POST['nama'],
             alamat = req.POST['alamat'],
             no_tlp = req.POST['no_tlp'],
         )
-        return redirect('/list_pengunjung/tamu')
+        return redirect('/list_pengunjung')
     return render(req,'list_pengunjung/edit.html', {'data':data})
 
 def profil_s(req):
